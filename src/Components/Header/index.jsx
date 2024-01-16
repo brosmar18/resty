161de6274652269ec11b3d38cbcbacefcaed35d7
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-16 09:46:02 -0600

    header styles updated. Responsive nav

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -4,38 +4,37 @@ import "./Header.scss";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
-
   return (
     <header className="header">
       <nav className="nav">
         <div className="nav__logo">
           <img src="assets/logo.png" alt="logo" />
-          <ul className="nav__links">
-            {["home", "history", "help"].map((item) => (
-              <li className="nav__link" key={`link==-${item}`}>
-                <div />
-                <a href={`${item}`}>{item}</a>
-              </li>
-            ))}
-          </ul>
-          <div className="nav__mobile">
-            <HiMenuAlt4 onClick={() => setToggle(true)} />
+        </div>
+        <ul className="nav__links">
+          {["home", "history", "help"].map((item) => (
+            <li className="nav__link" key={`link-${item}`}>
+              <div />
+              <a href={`${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="nav__mobile">
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
 
-            {toggle && (
-              <div>
-                <HiX onClick={() => setToggle(false)} />
-                <ul>
-                  {["home", "history", "help"].map((item) => (
-                    <li key={item}>
-                      <a href={`${item}`} onClick={() => setToggle(false)}>
-                        {item}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </div>
+          {toggle && (
+            <div>
+              <HiX onClick={() => setToggle(false)} />
+              <ul>
+                {["home", "history", "help"].map((item) => (
+                  <li key={item}>
+                    <a href={`${item}`} onClick={() => setToggle(false)}>
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </header>
