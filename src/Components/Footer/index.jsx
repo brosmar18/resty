ffc8a8f5aaf30ea980c57a583e70df614505a92e
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-16 09:51:32 -0600

    footer, global styles updated

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,23 @@
-import './Footer.scss';
+import { FaFacebook, FaInstagram, FaGithubAlt, FaLinkedin } from "react-icons/fa";
+
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer className='footer'>&copy; 2018</footer>
-  )
-}
+    <footer className="footer">
+        <h3 className="title">RESTy</h3>
+        <div className="brand__copyright">
+          <p>&copy; 2018</p>
+          <p>All rights reserved</p>
+        </div>
+        <div className="brand__socials">
+          <FaFacebook />
+          <FaInstagram />
+          <FaGithubAlt />
+          <FaLinkedin />
+        </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
