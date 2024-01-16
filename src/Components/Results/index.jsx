a393dd79405cc6545ca9cfeca05ec47d6a6b7259
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-16 09:48:00 -0600

    results styles updated

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,9 +1,13 @@
 import './Results.scss';
 
-const Results = ({ data }) => {
+const Results = ({ data, requestParams }) => {
   return (
-    <section className='results'>
-      <pre>{data ? JSON.stringify(data, undefined, 2) : null }</pre>
+    <section className="results">
+      <div className='results__text'>
+        <p><span className="results__method-label">Method:</span> {requestParams.method}</p>
+        <p><span className="results__url-label">URL:</span> {requestParams.url}</p>
+      </div>
+      <code>{data ? JSON.stringify(data, undefined, 2) : null}</code>
     </section>
   )
 }
