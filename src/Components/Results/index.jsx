8daba8bc4141e4cc1c08959b98e05c4213f9035c
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-19 17:18:35 -0600

    Results tests passing

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -2,20 +2,20 @@ import './Results.scss';
 
 const Results = ({ data, requestParams, isLoading }) => {
   return (
-    <section className="results">
+    <section className="results" data-testid="results-section">
       {isLoading ? (
-        <p>Loading...</p>
+        <p data-testid="loading">Loading...</p>
       ) : (
         <>
-          <div className='results__text'>
-            {requestParams.method && <p><span className="results__method-label">Method:</span> {requestParams.method}</p>}
-            {requestParams.url && <p><span className="results__url-label">URL:</span> {requestParams.url}</p>}
+          <div className='results__text' data-testid="results-text">
+            {requestParams.method && <p data-testid="method"><span className="results__method-label">Method:</span> {requestParams.method}</p>}
+            {requestParams.url && <p data-testid="url"><span className="results__url-label">URL:</span> {requestParams.url}</p>}
           </div>
-          <code>{data ? JSON.stringify(data, null, 2) : 'No Data'}</code>
+          <code data-testid="result-data">{data ? JSON.stringify(data, null, 2) : 'No Data'}</code>
         </>
       )}
     </section>
-  )
+  );
 }
 
 export default Results;
