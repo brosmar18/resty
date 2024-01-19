7a7744bb817ed462be35398012d99fd507729a3c
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-19 17:09:33 -0600

    Header tests passing

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -4,29 +4,30 @@ import "./Header.scss";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+
   return (
-    <header className="header">
+    <header className="header" data-testid="header">
       <nav className="nav">
-        <div className="nav__logo">
+        <div className="nav__logo" data-testid="logo">
           <img src="assets/logo.png" alt="logo" />
         </div>
         <ul className="nav__links">
           {["home", "history", "help"].map((item) => (
-            <li className="nav__link" key={`link-${item}`}>
+            <li className="nav__link" key={`link-${item}`} data-testid={`nav-link-${item}`}>
               <div />
               <a href={`${item}`}>{item}</a>
             </li>
           ))}
         </ul>
-        <div className="nav__mobile">
-          <HiMenuAlt4 onClick={() => setToggle(true)} />
+        <div className="nav__mobile" data-testid="nav-mobile">
+          <HiMenuAlt4 onClick={() => setToggle(true)} data-testid='menu-icon' />
 
           {toggle && (
-            <div>
-              <HiX onClick={() => setToggle(false)} />
+            <div data-testid="mobile-menu">
+              <HiX onClick={() => setToggle(false)} data-testid='close-icon' />
               <ul>
                 {["home", "history", "help"].map((item) => (
-                  <li key={item}>
+                  <li key={item} data-testid={`mobile-nav-link-${item}`}>
                     <a href={`${item}`} onClick={() => setToggle(false)}>
                       {item}
                     </a>
