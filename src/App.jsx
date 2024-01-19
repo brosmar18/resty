449fc5bcb65cb3421408c32467dc43b84608bca8
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-18 18:49:59 -0600

    App converted to functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
 import Header from './Components/Header';
@@ -6,44 +6,39 @@ import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
 
-class App extends React.Component {
+const App = () => {
+  const [data, setData] = useState(null);
+  const [requestParams, setRequestParams] = useState({});
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
-  }
+  const callApi = (newRequestParams) => {
+    setRequestParams(newRequestParams);
 
-  callApi = (requestParams) => {
-    this.setState({requestParams});
-
-    if (requestParams.url) {
-      fetch(requestParams.url).then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error('Network response was not ok.');
-      }).then(data => {
-        this.setState({ data });
-      }).catch(error => {
-        console.error('There has been a problem with the fetch operation: ', error);
-        this.setState({ data: null });
-      })
+    if (newRequestParams.url) {
+      fetch(newRequestParams.url)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Network response was not ok.');
+        })
+        .then(newData => {
+          setData(newData);
+        })
+        .catch(error => {
+          console.error('There has been a problem with the fetch operation: ', error);
+          setData(null);
+        });
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className='app'>
-        <Header />
-        <Form handleApiCall={this.callApi} />
-        <Results data={this.state.data} requestParams={this.state.requestParams} />
-        <Footer />
-      </div>
-    );
-  }
-}
+  return (
+    <div className='app'>
+      <Header />
+      <Form handleApiCall={callApi} />
+      <Results data={data} requestParams={requestParams} />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
