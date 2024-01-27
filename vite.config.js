edaaca7187c925ce77f409ea9256bb18a705d43a
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-27 14:08:57 -0600

    shadcn installed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
