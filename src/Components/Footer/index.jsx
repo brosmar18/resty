44ca27659e087f84abeb03f434491d5ca99fdc4f
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-27 14:35:33 -0600

    Footer updated

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -4,16 +4,10 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer" data-testid="footer">
-        <h3 className="title" data-testid="footer-title">RESTy</h3>
-        <div className="brand__copyright" data-testid="copyright">
-          <p>&copy; 2018</p>
-          <p>All rights reserved</p>
-        </div>
-        <div className="brand__socials" data-testid="socials">
-          <FaFacebook />
-          <FaInstagram />
-          <FaGithubAlt />
-          <FaLinkedin />
+        <div className="footer__container">
+          <p className="footer___copyright">
+            &copy; RESTy by Bryan O. Garduno Gonzalez
+          </p>
         </div>
     </footer>
   );
