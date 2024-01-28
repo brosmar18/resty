effcecb69dedd7682a3f6a2cd217dcdf60c36590
brosmar18 <bryangonzalez1896@icloud.com>
2024-01-28 13:56:28 -0600

    nonexistent routes removed

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -17,25 +17,25 @@ const Sidebar = () => {
           <SiElement size={25} />
           <p className="sidebar__link-title">App</p>
         </Link>
-        <Link to="/user" className="sidebar__link" data-testid="sidebar-link-user">
+        <Link to="/" className="sidebar__link" data-testid="sidebar-link-user">
           <FaUser size={25} />
           <p className="sidebar__link-title">User</p>
         </Link>
       </div>
       <div className="sidebar__item" data-testid="sidebar-general">
         <h2 className="sidebar__item-title">General</h2>
-        <Link to="/notes" className="sidebar__link" data-testid="sidebar-link-notes">
+        <Link to="/" className="sidebar__link" data-testid="sidebar-link-notes">
           <FaEdit size={25} />
           <p className="sidebar__link-title">Notes</p>
         </Link>
-        <Link to="/calendar" className="sidebar__link" data-testid="sidebar-link-calendar">
+        <Link to="/" className="sidebar__link" data-testid="sidebar-link-calendar">
           <FaCalendarAlt size={25} />
           <p className="sidebar__link-title">Calendar</p>
         </Link>
       </div>
       <div className="sidebar__item" data-testid="sidebar-maintenance">
         <h2 className="sidebar__item-title">Maintenance</h2>
-        <Link to="/settings" className="sidebar__link" data-testid="sidebar-link-settings">
+        <Link to="/" className="sidebar__link" data-testid="sidebar-link-settings">
           <IoMdSettings size={25} />
           <p className="sidebar__link-title">Settings</p>
         </Link>
